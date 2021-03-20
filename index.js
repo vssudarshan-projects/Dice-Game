df06ec025cd41e8cb94cf31932a4ed317c59fe07
SudarshanVS <40467213+SudarshanVS@users.noreply.github.com>
2021-03-20 21:02:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 document.querySelector("#click-btn").onclick = () => {
 
-var img = document.querySelectorAll("img");
-var rnd = [];
+  var img = document.querySelectorAll("img");
+  var rnd = [];
 
-for(var i = 0;i < 2; i++){
-rnd.push((Math.floor(Math.random() * 6) + 1));
-img[i].src = "./images/dice" + rnd[i] + ".png";
-}
+  for(var i = 0; i < 2 ; i++){
+    rnd.push((Math.floor(Math.random() * 6) + 1));
+    img[i].src = "./images/dice" + rnd[i] + ".png";
+  }
 
-if(rnd[0]>rnd[1]){
-document.querySelector("h1").textContent = "Player 1 Wins!";
-document.querySelector("#player1-text").style.color = "#FA4A37";
-document.querySelector("#player2-text").style.color = "#259934";
-}else if(rnd[0]<rnd[1]){
-document.querySelector("h1").textContent = "Player 2 Wins!";
-document.querySelector("#player2-text").style.color = "#FA4A37";
-document.querySelector("#player1-text").style.color = "#259934";
-}else{
-document.querySelector("h1").textContent = "Draw!";
-document.querySelector("#player1-text").style.color = "#259934";
-document.querySelector("#player2-text").style.color = "#259934";
-}
+  if(rnd[0]>rnd[1]){
+    document.querySelector("h1").textContent = "Player 1 Wins!";
+    document.querySelector("#player1-text").style.color = "#FA4A37";
+    document.querySelector("#player2-text").style.color = "#259934";
+  }else if(rnd[0]<rnd[1]){
+    document.querySelector("h1").textContent = "Player 2 Wins!";
+    document.querySelector("#player2-text").style.color = "#FA4A37";
+    document.querySelector("#player1-text").style.color = "#259934";
+  }else{
+    document.querySelector("h1").textContent = "Draw!";
+    document.querySelector("#player1-text").style.color = "#259934";
+    document.querySelector("#player2-text").style.color = "#259934";
+  }
 }
